c8b27223cdbbce580fe761b25c2513552841e890
jeff5343 <hongjefferson211@gmail.com>
2023-01-25 19:46:57 -0800

    Implement parsing entity types

--- src/game_data/entity_types.rs
+++ src/game_data/entity_types.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-use serde_json::Value;
+use heck::AsUpperCamelCase;
+use serde_json::{Map, Value};
 
 use super::Directory;
 
@@ -12,18 +13,52 @@ pub struct EntityTypes {
 
 impl EntityTypes {
     pub fn parse(game_data: &Value) -> EntityTypes {
-        let category_to_entity_types = HashMap::new();
+        let mut category_to_entity_types = HashMap::new();
+
+        ENTITY_TYPE_CATEGORIES.iter().for_each(|category| {
+            category_to_entity_types.insert(
+                category.to_string(),
+                match game_data.get(category) {
+                    Some(category_data) => entity_types_from_category_data(&category_data),
+                    None => Vec::new(),
+                },
+            );
+        });
+
         EntityTypes {
-            category_to_entity_types: category_to_entity_types
+            category_to_entity_types: category_to_entity_types,
         }
     }
+}
 
-    pub fn from_category(&self, category: &str) -> Option<&Vec<EntityType>> {
-        self.category_to_entity_types.get(category)
-    }
+fn entity_types_from_category_data(category_data: &Value) -> Vec<EntityType> {
+    category_data
+        .as_object()
+        .unwrap_or(&Map::new())
+        .iter()
+        .map(|(_, entity_type)| EntityType {
+            name: entity_type
+                .get("name")
+                .unwrap_or(&Value::String(String::from("Undefined")))
+                .as_str()
+                .unwrap()
+                .to_string(),
+            scripts: Directory::parse(
+                entity_type
+                    .get("scripts")
+                    .unwrap_or(&Value::Array(Vec::new())),
+            ),
+        })
+        .collect()
 }
 
 pub struct EntityType {
     name: String,
     scripts: Directory,
+}
+
+impl EntityType {
+    fn class_name(&self) -> String {
+        AsUpperCamelCase(self.name.clone()).to_string()
+    }
 }
