80fa2368beaf9a0d5ce5461262dcd7c8e6dfe83e
Jeff <hongjefferson211@gmail.com>
2023-04-02 19:36:54 -0700

    Fix publishing pymodd library onto pypi

--- pymodd/console_scripts/generate_project.py
+++ pymodd/console_scripts/generate_project.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import pymodd_generator
+from pymodd import _pymodd_generator
 
 
 parser = ArgumentParser(
@@ -9,7 +9,7 @@ parser.add_argument('json_file_path', help='the path of the modd.io json file')
 
 def main():
     json_file_path = parser.parse_args().json_file_path
-    pymodd_generator.generate_project_from_json_file_path(json_file_path)
+    _pymodd_generator.generate_project_from_json_file_path(json_file_path)
 
 
 if __name__ == '__main__':
