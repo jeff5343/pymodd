0c5553204093485e283b1cad0e8ab05435568eb2
Jeff <hongjefferson211@gmail.com>
2023-05-20 17:01:42 -0700

    Add `default-template` option for `generate-project` command

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,13 @@
 mod game_data;
 mod project_generator;
 
-use std::fs;
-
 use crossterm::style::Stylize;
 use game_data::GameData;
 use project_generator::ProjectGenerator;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn generate_project_from_json_file_path(json_file_path: String) {
-    let json_file_content = match fs::read_to_string(&json_file_path) {
-        Err(_) => {
-            log_error("invalid json file path!");
-            return;
-        }
-        Ok(content) => content,
-    };
-
+fn generate_project_from_json_file_content(json_file_content: String) {
     let game_data = match GameData::parse(json_file_content) {
         Err(err_msg) => {
             log_error(err_msg);
@@ -70,7 +60,10 @@ pub fn log_cli_end_message(completed_action: &str, ended_successfully: bool) {
 
 #[pymodule]
 fn _pymodd_helper(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(generate_project_from_json_file_path, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        generate_project_from_json_file_content,
+        m
+    )?)?;
     m.add_function(wrap_pyfunction!(log_success, m)?)?;
     m.add_function(wrap_pyfunction!(log_error, m)?)?;
     m.add_function(wrap_pyfunction!(log_cli_start_message, m)?)?;
